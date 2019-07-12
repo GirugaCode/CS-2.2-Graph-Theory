a4ed63f84c868bf84204d6768d0b0203e266a51a
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-07-12 00:03:10 -0700

    [update] Able to print the amount of verticies and edges but working on weights

--- Challenges/challenge-1.py
+++ Challenges/challenge-1.py
@@ -1,13 +1,45 @@
 #!python3
 
 class Graph:
-    def __init__(self, verticies, edges, weight):
+    def __init__(self, verticies, edges):
         self.verticies = verticies
         self.edges = edges
-        self.weight = weight
-    
-    def read_text(self, textfile):
-        file = open("graph.txt", "r")
-        return file
+        # self.weight = weight
+    def __str__(self):
 
-    print(read_text)
+        return "Vertices: {} \nEdges: {}".format(self.verticies, self.edges)
+
+    # def __repr__(self):
+    #     return "Vertices: %s\nEdges: %s" % (len(self.verticies), len(self.edges))
+
+    def _edges_weight(self):
+        # ['(1,2,10)', '(1,4,20)', '(2,3,50)', '(2,4,11)']
+
+        # weights = {
+        #     (0, 2): 4,
+        #     (0, 4): 60,
+        #     (0, 3): 23,
+        #     (2, 3): 4,
+        #     (3, 1): 10,
+        #     (4, 2): 15,
+        # }
+        edges_with_weight = self.edges
+        print(edges_with_weight)
+        
+
+
+
+
+def graph_text(text_file):
+    with open(text_file, "r") as open_file:
+        text = open_file.read()
+        clean_text = text.split("\n")
+        
+        graph = Graph(clean_text[1].split(","), clean_text[2:])
+        print(graph._edges_weight())
+        # return Graph(clean_text[1].split(","), clean_text[2:])
+
+print(graph_text("graph-data.txt"))
+
+
+
